a4de87c6b400f32262cc8a2f5b71eacc0910b6b5
CubedLeaves <105320711+DarkTerraYT@users.noreply.github.com>
2023-08-20 21:38:10 -0400

    Fixed two more typos

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,8 +6,8 @@ public static class ModHelperData
     public const string Version = "2.0.0";
     public const string Name = "Customizable Tower";
 
-    public const string Description = "Adds a Completely Customizeable Tower to The Game. You Can Customize it's Stats in The Config For The Mod.\n\nPlease don't use this in competitive modes, one: it could get your account flagged, and two: it ruins the fun for others.";
+    public const string Description = "Adds a Completely Customizable Tower to The Game. You Can Customize it's Stats in The Config For The Mod.\n\nPlease don't use this in competitive modes, one: it could get your account flagged, and two: it ruins the fun for others.";
 
     public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "CustomizeableTower"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoName = "CustomizableTower"; // TODO add your repo name here, also in the download url in README.md
 }
