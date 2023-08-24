049e7da6366484d375ee4d3220aeffe0997b16f5
DarkTerraYT <liamwolfguy@gmail.com>
2023-08-24 17:20:44 -0400

    Updated small things

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,10 +3,10 @@ namespace CustomizableTower;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "37.3";
-    public const string Version = "2.1.0";
+    public const string Version = "2.1.1";
     public const string Name = "Customizable Tower";
 
-    public const string Description = "Adds a Completely Customizable Tower to The Game. You Can Customize it's Stats in The Config For The Mod.\n\nPlease don't use this in competitive modes, one: it could get your account flagged, and two: it ruins the fun for others.";
+    public const string Description = "Adds a Completely Customizable Tower to The Game. You Can Customize it's Stats in The Config For The Mod.\n\nPlease don't use this in competitive modes, one: it could get your account flagged, and two: it ruins the fun for others.\n Join the Discord! https://discord.gg/xegnVEBRuE";
 
     public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "CustomizableTower"; // TODO add your repo name here, also in the download url in README.md
