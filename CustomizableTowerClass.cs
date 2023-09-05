cbdba05d212b21f75af5c1f08fec2e144b0f8154
DarkTerraYT <liamwolfguy@gmail.com>
2023-09-05 18:53:03 -0400

    Added Weapon Copying and Cash Generation

--- CustomizableTowerClass.cs
+++ CustomizableTowerClass.cs
@@ -5,6 +5,10 @@ using Il2CppAssets.Scripts.Models.Towers;
 using Il2CppAssets.Scripts.Models.TowerSets;
 using BTD_Mod_Helper.Extensions;
 using Il2CppAssets.Scripts.Models.Towers.Behaviors.Emissions;
+using Il2CppAssets.Scripts.Unity;
+using Il2CppAssets.Scripts.Models.Towers.Behaviors.Attack;
+using Il2CppAssets.Scripts.Models.Towers.Projectiles.Behaviors;
+using Il2CppAssets.Scripts.Models.Towers.Weapons.Behaviors;
 
 namespace CustomizableTower
 {
@@ -29,60 +33,136 @@ namespace CustomizableTower
 
         public override void ModifyBaseTowerModel(TowerModel towerModel)
         {
-            towerModel.ApplyDisplay<CustomizedTowerDisplay>();
-            towerModel.range = CustomizableTower.Range;
-            towerModel.GetWeapon().projectile.pierce = CustomizableTower.Pierce;
-            towerModel.GetWeapon().rate = CustomizableTower.Speed;
-            towerModel.GetAttackModel().range = CustomizableTower.Range;
-            var proj = towerModel.GetWeapon().projectile;
-            proj.ApplyDisplay<CustomizedProjectileDisplay>();
-            proj.GetDamageModel().damage = CustomizableTower.Damage;
-            var DamageModel = proj.GetDamageModel();
-            if (CustomizableTower.HitAll)
+            if(CustomizableTower.CopyAttackModel)
             {
-                proj.GetDamageModel().immuneBloonProperties = Il2Cpp.BloonProperties.None;
+                towerModel.GetWeapon(0).rate = 9999999999999999999;
+                towerModel.AddBehavior(Game.instance.model.GetTowerFromId(CustomizableTower.CopyAttackModelString));
+                towerModel.ApplyDisplay<CustomizedTowerDisplay>();
+                towerModel.range = CustomizableTower.Range;
+                towerModel.GetWeapon(1).projectile.pierce = CustomizableTower.Pierce;
+                towerModel.GetWeapon(1).rate = CustomizableTower.Speed;
+                towerModel.GetAttackModel(1).range = CustomizableTower.Range;
+                var proj = towerModel.GetWeapon(1).projectile;
+                proj.ApplyDisplay<CustomizedProjectileDisplay>();
+                proj.GetDamageModel().damage = CustomizableTower.Damage;
+                var DamageModel = proj.GetDamageModel();
+                if (CustomizableTower.HitAll)
+                {
+                    proj.GetDamageModel().immuneBloonProperties = Il2Cpp.BloonProperties.None;
+                }
+                else if (!CustomizableTower.HitLead && !CustomizableTower.HitPurple && !CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Lead | Il2Cpp.BloonProperties.Purple | Il2Cpp.BloonProperties.Frozen;
+                }
+                else if (!CustomizableTower.HitLead && !CustomizableTower.HitPurple && CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Purple | Il2Cpp.BloonProperties.Lead;
+                }
+                else if (CustomizableTower.HitLead && !CustomizableTower.HitPurple && !CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Purple | Il2Cpp.BloonProperties.Frozen;
+                }
+                else if (!CustomizableTower.HitLead && CustomizableTower.HitPurple && !CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Lead | Il2Cpp.BloonProperties.Frozen;
+                }
+                else if (CustomizableTower.HitLead && !CustomizableTower.HitPurple && CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Purple;
+                }
+                else if (CustomizableTower.HitLead && CustomizableTower.HitPurple && !CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Frozen;
+                }
+                else if (!CustomizableTower.HitLead && CustomizableTower.HitPurple && CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Lead;
+                }
+                else if (CustomizableTower.HitLead && CustomizableTower.HitPurple && CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.None;
+                }
+                towerModel.GetWeapon(1).emission = new ArcEmissionModel("Emission", CustomizableTower.MultishotNumber + CustomizableTower.MultishotNumber, CustomizableTower.MultiShotOffset, CustomizableTower.MultiShotRotation, null, false, false);
             }
-            else if (!CustomizableTower.HitLead && !CustomizableTower.HitPurple && !CustomizableTower.HitFrozen)
+            else if (!CustomizableTower.CopyAttackModel)
             {
-                DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Lead | Il2Cpp.BloonProperties.Purple | Il2Cpp.BloonProperties.Frozen;
+                towerModel.ApplyDisplay<CustomizedTowerDisplay>();
+                towerModel.range = CustomizableTower.Range;
+                towerModel.GetWeapon().projectile.pierce = CustomizableTower.Pierce;
+                towerModel.GetWeapon().rate = CustomizableTower.Speed;
+                towerModel.GetAttackModel().range = CustomizableTower.Range;
+                var proj = towerModel.GetWeapon().projectile;
+                proj.ApplyDisplay<CustomizedProjectileDisplay>();
+                proj.GetDamageModel().damage = CustomizableTower.Damage;
+                var DamageModel = proj.GetDamageModel();
+                if (CustomizableTower.HitAll)
+                {
+                    proj.GetDamageModel().immuneBloonProperties = Il2Cpp.BloonProperties.None;
+                }
+                else if (!CustomizableTower.HitLead && !CustomizableTower.HitPurple && !CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Lead | Il2Cpp.BloonProperties.Purple | Il2Cpp.BloonProperties.Frozen;
+                }
+                else if (!CustomizableTower.HitLead && !CustomizableTower.HitPurple && CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Purple | Il2Cpp.BloonProperties.Lead;
+                }
+                else if (CustomizableTower.HitLead && !CustomizableTower.HitPurple && !CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Purple | Il2Cpp.BloonProperties.Frozen;
+                }
+                else if (!CustomizableTower.HitLead && CustomizableTower.HitPurple && !CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Lead | Il2Cpp.BloonProperties.Frozen;
+                }
+                else if (CustomizableTower.HitLead && !CustomizableTower.HitPurple && CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Purple;
+                }
+                else if (CustomizableTower.HitLead && CustomizableTower.HitPurple && !CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Frozen;
+                }
+                else if (!CustomizableTower.HitLead && CustomizableTower.HitPurple && CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Lead;
+                }
+                else if (CustomizableTower.HitLead && CustomizableTower.HitPurple && CustomizableTower.HitFrozen)
+                {
+                    DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.None;
+                }
+                towerModel.GetWeapon().emission = new ArcEmissionModel("Emission", CustomizableTower.MultishotNumber + CustomizableTower.MultishotNumber, CustomizableTower.MultiShotOffset, CustomizableTower.MultiShotRotation, null, false, false);
             }
-            else if (!CustomizableTower.HitLead && !CustomizableTower.HitPurple && CustomizableTower.HitFrozen)
-            {
-                DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Purple | Il2Cpp.BloonProperties.Lead;
-            }
-            else if (CustomizableTower.HitLead && !CustomizableTower.HitPurple && !CustomizableTower.HitFrozen)
-            {
-                DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Purple | Il2Cpp.BloonProperties.Frozen;
-            }
-            else if (!CustomizableTower.HitLead && CustomizableTower.HitPurple && !CustomizableTower.HitFrozen)
-            {
-                DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Lead | Il2Cpp.BloonProperties.Frozen;
-            }
-            else if (CustomizableTower.HitLead && !CustomizableTower.HitPurple && CustomizableTower.HitFrozen)
-            {
-                DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Purple;
-            }
-            else if (CustomizableTower.HitLead && CustomizableTower.HitPurple && !CustomizableTower.HitFrozen)
-            {
-                DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Frozen;
-            }
-            else if (!CustomizableTower.HitLead && CustomizableTower.HitPurple && CustomizableTower.HitFrozen)
-            {
-                DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.Lead;
-            }
-            else if (CustomizableTower.HitLead && CustomizableTower.HitPurple && CustomizableTower.HitFrozen)
-            {
-                DamageModel.immuneBloonProperties = Il2Cpp.BloonProperties.None;
-            }
-            if (CustomizableTower.SeeCamo)
+            if(CustomizableTower.SeeCamo)
             {
                 towerModel.GetDescendants<FilterInvisibleModel>().ForEach(model => model.isActive = false);
             }
-            else if (!CustomizableTower.SeeCamo)
+            if (!CustomizableTower.SeeCamo)
             {
                 towerModel.GetDescendants<FilterInvisibleModel>().ForEach(model => model.isActive = true);
             }
-            towerModel.GetWeapon().emission = new ArcEmissionModel("Emission", CustomizableTower.MultishotNumber + CustomizableTower.MultishotNumber, CustomizableTower.MultiShotOffset, CustomizableTower.MultiShotRotation, null, false, false);
+            if (CustomizableTower.GenerateMoney)
+            {
+                if (CustomizableTower.AutoCollectBananas)
+                {
+                    var generateCashModel = Game.instance.model.GetTowerFromId("BananaFarm003").GetAttackModel().Duplicate();
+                    var cashModel = generateCashModel.GetBehavior<CashModel>();
+                    cashModel.maximum = CustomizableTower.MaxBananaValue;
+                    cashModel.minimum = CustomizableTower.MinBananaValue;
+                    generateCashModel.weapons[0].GetBehavior<EmissionsPerRoundFilterModel>().count = CustomizableTower.BananasPerRound;
+                    towerModel.AddBehavior(generateCashModel);
+                }
+                else
+                {
+                    var generateCashModel = Game.instance.model.GetTowerFromId("BananaFarm000").GetAttackModel().Duplicate();
+                    var cashModel = generateCashModel.GetBehavior<CashModel>();
+                    cashModel.maximum = CustomizableTower.MaxBananaValue;
+                    cashModel.minimum = CustomizableTower.MinBananaValue;
+                    generateCashModel.weapons[0].GetBehavior<EmissionsPerRoundFilterModel>().count = CustomizableTower.BananasPerRound;
+                    towerModel.AddBehavior(generateCashModel);
+                }
+                
+            }
         }
         public override bool IsValidCrosspath(int[] tiers) =>
             ModHelper.HasMod("UltimateCrosspathing") ? true : base.IsValidCrosspath(tiers);
