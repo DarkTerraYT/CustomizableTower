c6716d9e3f79f030efe240db640cc06646c1ce6c
DarkTerraYT <liamwolfguy@gmail.com>
2023-09-08 21:38:35 -0400

    Updated Version

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace CustomizableTower;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "37.3";
-    public const string Version = "2.5.2";
+    public const string Version = "2.5.3";
     public const string Name = "Customizable Tower";
 
     public const string Description = "(If you play this mod please credit me as LerpmcGerk on YT and Discord, putting a link to my YT channel would help too!) Adds a Completely Customizable Tower to The Game. You Can Customize it's Stats in The Config For The Mod.\n\nPlease don't use this in competitive modes, one: it could get your account flagged, and two: it ruins the fun for others.\n Join the Discord! https://discord.gg/xegnVEBRuE";
