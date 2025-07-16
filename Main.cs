b104280852aaf480b47dc4dfa49e9fa8f50e0de3
liamw <liamwolfguy@gmail.com>
2025-07-16 00:36:38 -0400

    Delete old files

---
+++ Main.cs
@@ -0,0 +1,21 @@
+using BTD_Mod_Helper;
+using BTD_Mod_Helper.Api.ModOptions;
+using MelonLoader;
+using CustomizableTower;
+using System;
+using System.Reflection;
+using System.IO;
+using Il2CppAssets.Scripts.Unity.UI_New.Popups;
+using System.Linq;
+using UnityEngine;
+using Il2CppAssets.Scripts.Models.Towers.Projectiles;
+
+[assembly: MelonInfo(typeof(CustomizableTower.Main), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace CustomizableTower;
+
+public class Main : BloonsTD6Mod
+{
+    
+}
